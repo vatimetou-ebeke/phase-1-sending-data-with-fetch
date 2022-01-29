1a80a7e96f23da3a4e7cc4942cf931926642b980
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-01-28 19:55:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
 // Add your code here
-// const formData = {
-//     dogName: "Byron",
-//     dogBreed: "Poodle",
-//   };
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
   
-//   const configurationObject = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json",
-//     },
-//     body: JSON.stringify(formData),
-//   };
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
   
-//   fetch("http://localhost:3000/dogs", configurationObject)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (object) {
-//       console.log(object);
-//     }).catch(function (error) {
-//         alert("Bad things! Ragnarők!")
-//         console.log(error.message);
-//     })
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    }).catch(function (error) {
+        alert("Bad things! Ragnarők!")
+        console.log(error.message);
+    })
 
-     function submitData(str1, str2) {
-         fetch('http://localhost:3000/users',{
+     function submitData(username, useremail) {
+         const users={
+         name: username,
+         email: useremail
+         }
+
+        return fetch('http://localhost:3000/users',{
+            method:'POST',
+            headers: {
+                "Content-Type": "application/json",
+                  Accept: "application/json",
+
+            },
+            body:JSON.stringify(users)
 
          }).then(function (response) {
              return response.json();
          }).then(function (object) {
              console.log(object);
-         })
+            
+
+        
+        })
+        
      }
-     submitData("username", "useremail")
+    
 
